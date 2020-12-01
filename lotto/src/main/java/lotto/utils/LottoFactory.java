df37620d1a3af4fa02e71602032d614e16368ecc
ecsimsw <ecsimsw@gmail.com>
2020-12-02 05:46:24 +0900

    feat : 입력 개수만큼의 Lotto를 포함한, Lottos 객체 생성 기능 구현

---
+++ lotto/src/main/java/lotto/utils/LottoFactory.java
@@ -0,0 +1,39 @@
+package lotto.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lotto.lotto.Lotto;
+import lotto.lotto.Lottos;
+import lotto.lotto.WinningLotto;
+
+public class LottoFactory {
+    private LottoFactory(){}
+
+    public static Lottos createLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        while (lottos.size() < count) {
+            lottos.add(createRandomLotto());
+        }
+        return new Lottos(lottos);
+    }
+
+    private static Lotto createRandomLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < Lotto.LEN) {
+            numbers.add(getRandWithoutDuplicate(numbers));
+        }
+        return new Lotto(numbers);
+    }
+
+    private static int getRandWithoutDuplicate(List<Integer> numbers) {
+        int rand;
+
+        do {
+            rand = RandomUtils.nextInt(Lotto.MIN, Lotto.MAX);
+        } while (numbers.contains(rand));
+        return rand;
+    }
+}
