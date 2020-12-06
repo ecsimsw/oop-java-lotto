b0865fe7e498ab8628c8b94ad70b1baf339f5c1c
ecsimsw <ecsimsw@gmail.com>
2020-12-06 21:04:28 +0900

    feat : 로또 점수 기록 출력 테스트 코드 작성

---
+++ lotto/src/test/java/domain/LottoListTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoListTest {
+
+    @Test
+    void printWinningResult() {
+        LottoList lottoList = LottoFactory.getLottoList(500);
+        Lotto lotto = new Lotto(Arrays.asList(new Integer[]{1,2,3,4,5,6}));
+        WinningLotto winningLotto = new WinningLotto(lotto, 7);
+        lottoList.printWinningResult(winningLotto);
+    }
+}
