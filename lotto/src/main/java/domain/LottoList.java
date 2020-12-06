2f1143f23b31de22145e976a6a364e886aea98b7
ecsimsw <ecsimsw@gmail.com>
2020-12-06 09:55:58 +0900

    feat : 지정한 수 만큼의 Lotto 객체 생성, List<Lotto>를 포장한 LottoList 객체 생성 구현

---
+++ lotto/src/main/java/domain/LottoList.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.List;
+
+public class LottoList {
+
+    List<Lotto> lottoList;
+
+    public LottoList(List<Lotto> lottoList){
+        this.lottoList = lottoList;
+    }
+}
