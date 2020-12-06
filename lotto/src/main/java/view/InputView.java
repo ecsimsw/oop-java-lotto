721247f663e0c215aba91a0aab5bc91a7d25db58
ecsimsw <ecsimsw@gmail.com>
2020-12-06 10:39:33 +0900

    feat : 보너스 볼 입력 구현

--- lotto/src/main/java/view/InputView.java
+++ lotto/src/main/java/view/InputView.java
@@ -2,6 +2,7 @@ package view;
 
 import domain.Lotto;
 import domain.Money;
+import domain.WinningLotto;
 import utils.StringHandler;
 
 import java.util.Scanner;
@@ -10,6 +11,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String ASK_MONEY_MESSAGE = "구입금액을 입력해 주세요.\n";
     private static final String ASK_WINNER_NUMBER = "지난 주 당첨 번호를 입력해주세요.\n";
+    private static final String ASK_BONUS_BALL = "보너스 볼을 입력해주세요. \n";
 
     private InputView(){}
 
@@ -23,14 +25,31 @@ public class InputView {
         }
     }
 
-    public static Lotto getWinnerNumber(){
+    public static WinningLotto getWinningLotto(){
+        Lotto lotto = getLastLotto();
+        int bonus = getBonusBall();
+
+        return new WinningLotto(lotto, bonus);
+    }
+
+    private static Lotto getLastLotto(){
         try{
             OutputView.printMsg(ASK_WINNER_NUMBER);
             String line = getInput();
             return new Lotto(StringHandler.parseIntegerList(line, ","));
         }catch (IllegalArgumentException IAE){
             OutputView.printMsg(IAE.getMessage());
-            return getWinnerNumber();
+            return getLastLotto();
+        }
+    }
+
+    public static int getBonusBall(){
+        try{
+            OutputView.printMsg(ASK_BONUS_BALL);
+            return getInteger();
+        }catch (IllegalArgumentException IAE){
+            OutputView.printMsg(IAE.getMessage());
+            return getBonusBall();
         }
     }
 
