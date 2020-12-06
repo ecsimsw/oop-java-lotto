69749223ab1900fa94f1430394d086ddf7e98095
ecsimsw <ecsimsw@gmail.com>
2020-12-06 10:33:49 +0900

    feat : 지난 주 로또 번호 입력 구현

---
+++ lotto/src/main/java/utils/StringHandler.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringHandler {
+    private StringHandler(){}
+
+    public static List<Integer> parseIntegerList(String input, String delimiter){
+        return Arrays.stream(input.split(delimiter))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
--- lotto/src/main/java/view/InputView.java
+++ lotto/src/main/java/view/InputView.java
@@ -1,12 +1,15 @@
 package view;
 
+import domain.Lotto;
 import domain.Money;
+import utils.StringHandler;
 
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String ASK_MONEY_MESSAGE = "구입금액을 입력해 주세요.\n";
+    private static final String ASK_WINNER_NUMBER = "지난 주 당첨 번호를 입력해주세요.\n";
 
     private InputView(){}
 
@@ -17,6 +20,17 @@ public class InputView {
         }catch (IllegalArgumentException IAE){
             OutputView.printMsg(IAE.getMessage());
             return getMoney();
+        }
+    }
+
+    public static Lotto getWinnerNumber(){
+        try{
+            OutputView.printMsg(ASK_WINNER_NUMBER);
+            String line = getInput();
+            return new Lotto(StringHandler.parseIntegerList(line, ","));
+        }catch (IllegalArgumentException IAE){
+            OutputView.printMsg(IAE.getMessage());
+            return getWinnerNumber();
         }
     }
 
