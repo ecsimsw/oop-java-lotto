9409c0aecc0202dcb5308722b592007946bc56b9
ecsimsw <ecsimsw@gmail.com>
2020-12-06 22:10:53 +0900

    fix : 테스트 코드 수정

--- lotto/src/test/java/domain/LottoFactoryTest.java
+++ lotto/src/test/java/domain/LottoFactoryTest.java
@@ -2,13 +2,11 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class LottoFactoryTest {
 
     @Test
     void getLottoList() {
         LottoList lottoList = LottoFactory.getLottoList(5);
-        lottoList.print();
+        lottoList.printLottoList();
     }
 }
