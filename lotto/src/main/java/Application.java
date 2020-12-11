6dfd899ae4cde90c4ab58bbaf14c930090218b74
ecsimsw <ecsimsw@gmail.com>
2020-12-11 14:34:34 +0900

    feat : 우승 로또 입력 구현

--- lotto/src/main/java/Application.java
+++ lotto/src/main/java/Application.java
@@ -1,6 +1,13 @@
+import domain.Lotto;
+import domain.WinningLotto;
+import utils.LottoFactory;
 import view.In;
+import view.Out;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Application {
 
@@ -9,5 +16,25 @@ public class Application {
 
         int money = In.getMoney(scanner);
 
+        int lottoCnt = money / Lotto.PRICE;
+        int used = money - lottoCnt * Lotto.PRICE;
+
+        List<Lotto> lottoList = LottoFactory.getLottoList(lottoCnt);
+
+        Lotto lastLotto = In.getLastLotto(scanner);
+        int bonus = In.getBonusNumber(scanner);
+        createWinningLotto(lastLotto, bonus);
+
     }
+
+
+
+    public static WinningLotto createWinningLotto(Lotto lotto, int bonus){
+        WinningLotto winningLotto = new WinningLotto(lotto, bonus);
+        // bonus 숫자 범위
+        // bonus가 lotto에 포함되진 않는지 체크
+        return winningLotto;
+    }
+
+
 }
--- lotto/src/main/java/view/In.java
+++ lotto/src/main/java/view/In.java
@@ -1,8 +1,27 @@
 package view;
 
+import domain.Lotto;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class In {
+    public static Lotto getLastLotto(Scanner scanner){
+        Out.print("작년 로또 번호를 입력하세요.\n");
+        String line = In.getInput(scanner);
+        List<Integer> numbers = Arrays.stream(line.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return new Lotto(numbers);
+    }
+
+    public static int getBonusNumber(Scanner scanner){
+        Out.print("보너스 번호를 입력하세요. \n");
+        return Integer.parseInt(In.getInput(scanner));
+    }
+
     public static int getMoney(Scanner scanner){
         Out.print("로또 구입 금액을 입력해주세요.\n");
         try{
@@ -13,7 +32,7 @@ public class In {
         }
     }
 
-    private static String getInput(Scanner scanner){
+    public static String getInput(Scanner scanner){
         return scanner.nextLine();
     }
 }
